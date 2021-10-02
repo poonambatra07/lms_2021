d22dc0325b15bfa4d08c2ed345e75302647cd9fd
poonam <poonambatra07@gmail.com>
2021-10-02 16:19:56 +0530

    Fs and console

---
+++ demo.js
@@ -0,0 +1,16 @@
+var a=40;
+var b=30;
+var sum =a+b;
+console.log(sum);
+console.log("this is sum");
+
+function add(a,b){
+    console.log(a+b);
+
+}
+
+add(100,200);
+
+let sum1=(a,b)=> a+b;
+
+console.log(`this is sum ${sum1(20,30)}`);
